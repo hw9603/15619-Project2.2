89cb5a5174843264688809faedf34a6f390b01fb
Wen He <wenhe@andrew.cmu.edu>
2018-09-22 15:52:53 +0000

    task 2 checkpoint

---
+++ task2/playground_server/pyService.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, Response
+import json
+import os
+import sys
+from io import StringIO
+import logging
+import traceback
+import subprocess
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route("/py/eval", methods=['GET', 'POST'])
+def handle():
+    if request.method == 'POST':
+
+        # Implementation goes here.
+        #
+        # Both stdout and stderr should be captured.
+        # {"stdout": "Hello 15619!", "stderr": ""}
+        #
+        # Sample input: {"code": "print("Hello 15619!")"}
+
+        received_data = request.get_json()
+        code = received_data['code']
+
+        code_file = open("code.py", "w+")
+        code_file.write(code)
+        code_file.close()
+
+        proc = subprocess.Popen(['python3', 'code.py'],
+                                stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE)
+
+        try:
+            output = proc.communicate(timeout=15)
+            outs = output[0].decode("utf-8")
+            errs = output[1].decode("utf-8")
+        except TimeoutExpired:
+            proc.kill
+            output = proc.communicate()
+            outs = output[0].decode("utf-8")
+            errs = output[1].decode("utf-8")
+
+        ret = {"stderr": errs, "stdout": outs}
+        ret_js = json.dumps(ret, sort_keys=True)
+        response = Response(response=ret_js,
+                            status=200,
+                            mimetype='application/json')
+        return response
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True, host="0.0.0.0", port=6000)
