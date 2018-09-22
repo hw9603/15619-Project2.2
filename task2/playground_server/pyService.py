445c2fbaf8fa8e3bb85c81dbefced072c2e13c61
Wen He <wenhe@andrew.cmu.edu>
2018-09-22 06:46:05 +0000

    task 2 mid check

--- task2/playground_server/pyService.py
+++ task2/playground_server/pyService.py
@@ -1,4 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import json
+import os, sys
+from io import StringIO
+import logging
+import traceback
+import subprocess
 
 app = Flask(__name__, static_url_path='')
 
@@ -9,10 +15,63 @@ def handle():
         # Implementation goes here.
         #
         # Both stdout and stderr should be captured.
-        # {"stdout": "<output_from_stdout>", "stderr": "<output_from_stderr>"}
+        # {"stdout": "Hello 15619!", "stderr": ""}
+        #
+        # Sample input: {"code": "print("Hello 15619!")"}
 
         ### BEGIN STUDENT CODE ###
+        # stdout = sys.stdout
+        # stderr = sys.stderr
+        # sys.stdout = StringIO()
+        # sys.stderr = StringIO()
+        # redirected_output = sys.stdout
+        # redirected_error = sys.stderr
+
+        received_data = request.get_json()
+        code = received_data['code']
+
+        code_file = open("code.py", "w+")
+        code_file.write(code)
+        code_file.close()
+
+        proc = subprocess.Popen(['python3', 'code.py'], \
+                          stderr = subprocess.PIPE, \
+                          stdout = subprocess.PIPE)
+
+        try:
+            outs, errs = proc.communicate(timeout=15)
+        except TimeoutExpired:
+            proc.kill
+            outs, errs = proc.communicate()
+
+        # if outs == None:
+        #     outs = ""
+        # if errs == None:
+        #     errs = ""
+
+        # out, err, exc = None, None, None
+
+        # try:
+        #     exec(code)
+        # except:
+        #     exc = traceback.format_exc()
+
+        # out = redirected_output.getvalue()
+        # err = redirected_error.getvalue()
+
+        # sys.stdout = stdout
+        # sys.stderr = stderr
+
+        ret = {"stderr": errs, "stdout": outs}
+        ret_js = json.dumps(ret)
+        response = Response(response = ret_js,
+                            status = 200,
+                            mimetype = 'application/json')
+        return response
+
+
         ### END STUDENT CODE ###
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True, host="0.0.0.0", port=6000)
+
