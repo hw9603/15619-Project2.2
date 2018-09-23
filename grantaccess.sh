a2f2a7119b46c58c72fa3850461ca886bd248f8c
Wen He <wenhe@andrew.cmu.edu>
2018-09-23 22:01:03 +0000

    finally..

--- grantaccess.sh
+++ grantaccess.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-AKS_RESOURCE_GROUP=wenhetask4resource 
-AKS_CLUSTER_NAME=wenhetask4cluster
-ACR_RESOURCE_GROUP=wenhetask4resource
-ACR_NAME=wenhetask4registry
+AKS_RESOURCE_GROUP=wenhetask5resource
+AKS_CLUSTER_NAME=wenhetask5cluster
+ACR_RESOURCE_GROUP=wenhetask5resource
+ACR_NAME=wenhetask5registry
 
-# Get the id of the service principal configured for AKS 
-CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv) 
+# Get the id of the service principal configured for AKS
+CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
 
-# Get the ACR registry resource id 
-ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv) 
+# Get the ACR registry resource id
+ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
 
-# Create role assignment 
+# Create role assignment
 az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
 
