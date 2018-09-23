89c98e2a7858db41a6c7508210cb07492baff798
Wen He <wenhe@andrew.cmu.edu>
2018-09-23 03:49:09 +0000

    task 3 mid check

--- task3/playground_ui/playground.py
+++ task3/playground_ui/playground.py
@@ -1,27 +1,42 @@
-from flask import Flask, request
+from flask import Flask, request, Response, make_response, jsonify
+import requests
+import logging
+import json
 
-pythonServiceHostName = "http://<ip>";
+# pythonServiceHostName = "http://35.231.57.25/py/eval"
+pythonServiceHostName = "http://project22task3-frontend:80/py/eval"
 
 app = Flask(__name__, static_folder='site', static_url_path='')
+
 
 @app.route("/", methods=['GET'])
 def handle():
     return app.send_static_file("index.html")
 
+
 @app.route("/python", methods=['GET', 'POST'])
 def handlePython():
     if request.method == 'POST':
-        code = request.form['code']
-        print(code)
+        if request.headers['Content-Type'] == 'multipart/form-data':
+            code = request.form['code']
+
         # This should return the stdout and stderr in json format
-        # return the exact response fom pyService.py only!
+        # return the exact response from pyService.py only!
         # Your code should handle 'code' as an argument in both
         # request.form and request.json
-        ### BEGIN STUDENT CODE ###
-        ### END STUDENT CODE ###
-        return ''
+        if request.headers['Content-Type'] == 'application/json':
+            code = request.json['code']
+
+        req_body = {"code": code}
+        res = requests.post(pythonServiceHostName,
+                            data=json.dumps(req_body),
+                            headers={'Content-Type':
+                                     'application/json'})
+        res_json = jsonify(res.json())
+        return res_json
     else:
         return app.send_static_file("python.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
