9d5fddafcd06e69fcca647c5b63ab469c92e75c1
Wen He <wenhe@andrew.cmu.edu>
2018-09-23 06:47:52 +0000

    task 4 first check

--- task4/playground_ui/playground.py
+++ task4/playground_ui/playground.py
@@ -1,27 +1,50 @@
-from flask import Flask, request
+from flask import Flask, request, Response, make_response, jsonify
+import requests
+import logging
+import json
 
-pythonServiceHostName = "http://<ip>";
+# pythonServiceHostName = "http://35.185.94.101/py/eval"
+pythonServiceHostName = ["http://project22task4gcp:80/py/eval",
+                         "http://40.121.49.136/py/eval"]
+
+int index = 0
 
 app = Flask(__name__, static_folder='site', static_url_path='')
+
 
 @app.route("/", methods=['GET'])
 def handle():
     return app.send_static_file("index.html")
 
+
 @app.route("/python", methods=['GET', 'POST'])
 def handlePython():
     if request.method == 'POST':
-        code = request.form['code']
-        print(code);
+
         # This should return the stdout and stderr in json format
-        # return the exact response fom pyService.py only!
+        # return the exact response from pyService.py only!
         # Your code should handle 'code' as an argument in both
         # request.form and request.json
-        ### BEGIN STUDENT CODE ###
-        ### END STUDENT CODE ###
-        return ''
+        if request.headers['Content-Type'] == 'application/json':
+            code = request.json['code']
+        else:
+            code = request.form['code']
+        # if request.json is None:
+        #     code = request.form['code']
+        # else:
+        #     code = request.json['code']
+
+        req_body = {"code": code}
+        res = requests.post(pythonServiceHostName[index],
+                            data=json.dumps(req_body),
+                            headers={'Content-Type':
+                                     'application/json'})
+        res_json = jsonify(res.json())
+        index = 1 - index
+        return res_json
     else:
         return app.send_static_file("python.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
