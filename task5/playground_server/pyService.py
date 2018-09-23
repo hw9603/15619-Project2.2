40d06f1737b7e86e1285dfb998ba6ba7d0fce994
Wen He <wenhe@andrew.cmu.edu>
2018-09-23 21:49:10 +0000

    full scoregit add .

--- task5/playground_server/pyService.py
+++ task5/playground_server/pyService.py
@@ -1,6 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import json
+import os
+import sys
+from io import StringIO
+import logging
+import traceback
+import subprocess
 
 app = Flask(__name__, static_url_path='')
+
 
 @app.route("/py/eval", methods=['GET', 'POST'])
 def handle():
@@ -11,8 +19,35 @@ def handle():
         # Both stdout and stderr should be captured.
         # {"stdout": "<output_from_stdout>", "stderr": "<output_from_stderr>"}
 
-        ### BEGIN STUDENT CODE ###
-        ### END STUDENT CODE ###
+        # code = request.json['code']
+        received_data = request.get_json()
+        code = received_data['code']
+
+        code_file = open("code.py", "w+")
+        code_file.write(code)
+        code_file.close()
+
+        proc = subprocess.Popen(['python3', 'code.py'],
+                                stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE)
+
+        try:
+            output = proc.communicate(timeout=15)
+            outs = output[0].decode("utf-8")
+            errs = output[1].decode("utf-8")
+        except TimeoutExpired:
+            proc.kill
+            output = proc.communicate()
+            outs = output[0].decode("utf-8")
+            errs = output[1].decode("utf-8")
+
+        ret = {"stderr": errs, "stdout": outs}
+        ret_js = json.dumps(ret, sort_keys=True)
+        response = Response(response=ret_js,
+                            status=200,
+                            mimetype='application/json')
+        return response
+
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True, host="0.0.0.0", port=6000)
--- task5/playground_ui/playground.py
+++ task5/playground_ui/playground.py
@@ -1,27 +1,87 @@
-from flask import Flask, request
+from flask import Flask, request, Response, make_response, jsonify
+import requests
+import logging
+import json
+import os.path
 
-pythonServiceHostName = "http://<ip>";
+pythonServiceHostName = ["http://104.45.148.227/py/eval",
+                         "http://project22task5-gke:80/py/eval"]
 
 app = Flask(__name__, static_folder='site', static_url_path='')
+
 
 @app.route("/", methods=['GET'])
 def handle():
     return app.send_static_file("index.html")
 
+
 @app.route("/python", methods=['GET', 'POST'])
 def handlePython():
     if request.method == 'POST':
-        code = request.form['code']
-        print(code);
+        if not os.path.exists("serviceindex.txt"):
+            f = open("serviceindex.txt", "w+")
+            f.write("0")
+            f.close()
+        if not os.path.exists("0"):
+            f = open("0", "w+")
+            f.write("0")  # initial error count for server 0
+            f.close()
+        if not os.path.exists("1"):
+            f = open("1", "w+")
+            f.write("0")  # initial error count for server 1
+            f.close()
+
+        f = open("serviceindex.txt", "r")
+        index = f.read()
+        f.close()
+        f = open(str(1 - int(index)), "r")
+        error_cnt = int(f.read())
+        f.close()
+
+        if (error_cnt <= 10):
+            f = open("serviceindex.txt", "w+")
+            f.write(str(1 - int(index)))
+            f.close()
+
         # This should return the stdout and stderr in json format
-        # return the exact response fom pyService.py only!
+        # return the exact response from pyService.py only!
         # Your code should handle 'code' as an argument in both
         # request.form and request.json
-        ### BEGIN STUDENT CODE ###
-        ### END STUDENT CODE ###
-        return ''
+        if request.headers['Content-Type'] == 'application/json':
+            code = request.json['code']
+        else:
+            code = request.form['code']
+        # if request.json is None:
+        #     code = request.form['code']
+        # else:
+        #     code = request.json['code']
+
+        req_body = {"code": code}
+
+        status_code = 0
+        while (status_code != 200):
+            res = requests.post(pythonServiceHostName[int(index)],
+                                data=json.dumps(req_body),
+                                headers={'Content-Type':
+                                         'application/json'})
+            status_code = res.status_code
+            if status_code != 200:
+                f = open(index, "r")
+                error_cnt = int(f.read())
+                f.close()
+                f = open(index, "w+")
+                f.write(str(error_cnt + 1))
+            f = open("serviceindex.txt", "w+")
+            f.write(str(1 - int(index)))
+            f.close()
+            index = str(1 - int(index))
+
+        res_json = jsonify(res.json())
+
+        return res_json
     else:
         return app.send_static_file("python.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
